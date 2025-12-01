aede2fa371c513f92440875d1f49a73c291be882
shlomi levi <shlomi8801@gmail.com>
2025-12-01 11:07:48 +0200

    added deleteUser function

--- server/users.py
+++ server/users.py
@@ -1,12 +1,22 @@
 import database
 import log
+import hashlib
 
+HASHINGALGO = hashlib.sha512
 #tables: Users, Carmodules
-USERS_TABLE = ["Users",{"username":"varchar(255) not null","password":"varchar(255) not null","token":"varchar(50)"}]
+USERS_TABLE = ["Users",{"username":"varchar(255) not null","password":"varchar(255) not null","token":"varchar(128)"}]
 CARMODULES_TABLE = "Carmodules"
+def deleteUser(username:str=None,token:str=None,algo:str="exact")->bool:
+    args =searchUser(username,token,algo)
+    if len(args) !=1:
+        return False
+    args = {list(USERS_TABLE[1].keys())[x]: args[0][x+1] for x in range(len(args))}
+    database.Delete(USERS_TABLE[0],args)
+
 def addUser(username:str,password:str)->bool:
     database.AddTable(*USERS_TABLE)
-    return database.Insert(USERS_TABLE[0],{"username":username,"password":password,"token":""})
+    return database.Insert(USERS_TABLE[0],{"username":username,"password":HASHINGALGO(password.encode()).hexdigest(),"token":""})
+
 def searchUser(username:str=None,token:str=None,algo:str="exact")->str:
     """algo is [exact,like,contains]"""
     # database.AddTable(*USERS_TABLE)
@@ -21,7 +31,6 @@ def searchUser(username:str=None,token:str=None,algo:str="exact")->str:
         log.log(f"searchUser went wrong no algo:{algo}")
         return None
     return database.Search(USERS_TABLE[0],args)
-    
 if (__name__=="__main__"):
     addUser("shlomi","pass")
     print(searchUser("shlomi"))
