8881a61218f008d1aa85f7d51cd5dcde3d6d9d95
shlomi levi <shlomi8801@gmail.com>
2025-12-30 10:21:00 +0200

    formated documents

--- client-web/dashboard/index.js
+++ client-web/dashboard/index.js
@@ -1,43 +1,41 @@
-async function logout(){
-    var res = await fetch(API_URL+"/logout",{
-        method: "POST",
-          headers: {}
-    });
-    res = await res.json()
-     switch (res.code) {
-          case 1: //error
-            alert("error logging out")
-            break;
-          case 0: // success
-            document.location = "/"
-            break;
-          default:
-            alert("error logging out: unexpected code")
-        }
+async function logout() {
+  var res = await fetch(API_URL + "/logout", {
+    method: "POST",
+    headers: {},
+  });
+  res = await res.json();
+  switch (res.code) {
+    case 1: //error
+      alert("error logging out");
+      break;
+    case 0: // success
+      document.location = "/";
+      break;
+    default:
+      alert("error logging out: unexpected code");
+  }
 }
-function changewindow(){
-const container = document.getElementById("container")
-  switch(getCurrentHash()){
-    case "#clients":{
-container.src = "clients.html"
+function changewindow() {
+  const container = document.getElementById("container");
+  switch (getCurrentHash()) {
+    case "#clients": {
+      container.src = "clients.html";
       break;
     }
-    case "#users":{
-container.src = "users.html"
+    case "#users": {
+      container.src = "users.html";
       break;
     }
-    case "#about":{
-container.src = "about.html"
+    case "#about": {
+      container.src = "about.html";
       break;
     }
-    default:{
-      container.src = "dashboard.html"
+    default: {
+      container.src = "dashboard.html";
       break;
     }
   }
-   
 }
 window.addEventListener("hashchange", () => {
-changewindow()
+  changewindow();
 });
-
--- client-web/utils.js
+++ client-web/utils.js
@@ -1,16 +1,14 @@
 function setCookie(key, value) {
-      document.cookie = `${key}=${value}; path=/`; // might have alot of problems here with vulnerabilities
-    }
-    function getCookie(key) {
-      let cookies = document.cookie;
-      return cookies
-        .substring(cookies.findIndex(key) + key.length)
-        .split(";")[0];
-    }
-    function deleteCookie(key) {
-      document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
-    }
-  function getCurrentHash(){
-    // the fragment of the url for example: "https://example.com/#hello" -> hello
-    return (new URL(window.location.href)).hash
-  }
+  document.cookie = `${key}=${value}; path=/`; // might have alot of problems here with vulnerabilities
+}
+function getCookie(key) {
+  let cookies = document.cookie;
+  return cookies.substring(cookies.findIndex(key) + key.length).split(";")[0];
+}
+function deleteCookie(key) {
+  document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
+}
+function getCurrentHash() {
+  // the fragment of the url for example: "https://example.com/#hello" -> hello
+  return new URL(window.location.href).hash;
+}
