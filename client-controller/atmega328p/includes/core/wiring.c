8d44388a6906f840c2bfeecf52ebda8aca191eaf
shlomi levi <shlomi8801@gmail.com>
2026-01-07 19:18:18 +0200

    fixed compiling by removing yield function from the delay function

--- client-controller/atmega328p/includes/core/wiring.c
+++ client-controller/atmega328p/includes/core/wiring.c
@@ -108,7 +108,8 @@ void delay(unsigned long ms)
 	uint32_t start = micros();
 
 	while (ms > 0) {
-		yield();
+		// yield(); removed because of an error  while compiling 
+		//i think its for running tasks in parallel 
 		while ( ms > 0 && (micros() - start) >= 1000) {
 			ms--;
 			start += 1000;
