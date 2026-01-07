a3a1b205b475f69dd1cdef2c8dd77bc8068b2f69
shlomi levi <shlomi8801@gmail.com>
2026-01-07 09:02:44 +0200

    moved atmega328p defines to client.cpp

--- client-controller/client.cpp
+++ client-controller/client.cpp
@@ -1,13 +1,13 @@
 //my arduino chip is atmega328p
 #define mmcu atmega328
 #define __AVR_ATmega328P__
+#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <Arduino.h>
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 #include <util/delay.h>
 // #include <eeprom.h> // used to use the arduino rom(as much as i know)
 
-SoftwareSerial test = {0,1};
 inline void sleep(unsigned int ms){ // inline because _delay_ms throws error when its non inline function
     _delay_ms(ms);
 }
@@ -16,7 +16,10 @@ void toggleLed(){
 }
 int main()
 {
-    test.begin(115200);
+    #ifdef UBRRH
+    gggg
+    #endif
+    // Serial.begin(115200);
     
     // Set built-in LED pin as output
     DDRB |= (1 << DDB5); // just sets the pb register at bit 5 which is the led state to output for output
@@ -24,7 +27,7 @@ int main()
       
     while (1) {
         toggleLed();
-        test.print("hello");
+        // Serial.print("hello");
         // PORTB = 1<<PORT5;
         // PORTD = 0b00000100;
         sleep(500);
