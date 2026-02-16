5a54f2db5377423d8b92e2888689419d37a6e5b6
shlomi levi <shlomi8801@gmail.com>
2026-02-16 10:27:50 +0200

    added some at commands and configuration to ATcommands.h

--- client-controller/ATcommands.h
+++ client-controller/ATcommands.h
@@ -1,5 +1,15 @@
 #pragma once
 
+//the modem starts at IP INITIAL status then requires to set apn with AT+CSTT or something
+//to switch to IP START then you can use the AT+CIICR then if successful switches to IP GPRSAC status
+
+#define APNNAME "internetg"
 #define GETSUBSCRIPTIONNUMBER "AT+CNUM"
 #define TESTCOMMAND "AT"
+#define STARTTCPCONNECTION "AT+CIPSTART" // section 8 in the document
 #define ATCONSOLESPEED 115200
+#define QUALITYREPORTCMD "AT+CSQ" // 3.2.16
+//https://github.com/DFRobot/DFRobot_SIM/blob/master/DFRobot_SIMclient.cpp might be helpful
+#define BRINGUPWIRELESSCONNECTIONGPRS "AT+CIICR" // 8.2.10
+#define GETCURRSTATUSCMD "AT+CIPSTATUS" // 8.2.13 
+#define SETAPNCMD "AT+CSTT" // add = at the end to set the apn 5.2.27
--- client-controller/client.cpp
+++ client-controller/client.cpp
@@ -9,37 +9,44 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <util/delay.h>
-
 // #include <eeprom.h> // used to use the arduino rom(as much as i know)
-
-// #include "avr/io.h"
-// #include "atmega328p/includes/core/Arduino.h"
-// #include "util/delay.h"
+#include "ATcommands.h"
 inline void sleep(unsigned int ms){ // inline because _delay_ms throws error when its non inline function
     _delay_ms(ms);
 }
 void toggleLed(){
     PORTB ^= 1<<PORTB5;
 }
+SoftwareSerial sim(2,3);
 int main()
 {
     Serial.begin(115200);
     Serial.println("hello");
     Serial.flush();
+    sim.begin(ATCONSOLESPEED);
     // analogWrite(PB0,1);
     // Set built-in LED pin as output
     DDRB |= (1 << DDB5); // just sets the pb register at bit 5 which is the led state to output for output
     DDRD = ~(0); //set pd2 to output  
-      sei(); // makes interrupts available(but says it might be faster to make the interrupts our self's)
-    while (1) {
-        toggleLed();
-        if(Serial.available()>0){Serial.println(Serial.read());
-        Serial.flush();}
-        
-        PORTB ^= 1<<PORT5;
-        // PORTD = 0b00000100;
-        sleep(0);
-        
-    }
+    sei(); // makes interrupts available(but says it might be faster to make the interrupts our self's)
+    
+    
+    
+       Serial.println(
+      F("***********************************************************"));
+  Serial.println(F(" You can now send AT commands"));
+  Serial.println(
+      F(" Enter \"AT\" (without quotes), and you should see \"OK\""));
+  Serial.println(
+      F(" If it doesn't work, select \"Both NL & CR\" in Serial Monitor"));
+  Serial.println(
+      F("***********************************************************"));
+
+  while (true) {
+    // if (Serial.available()) { sim.write(Serial.read()); Serial.write("ye");}
+    // if (sim.available()) { Serial.write(sim.read()); }
+    sleep(100);
+    PORTD ^= 1<<PORT5;
+  }
     return 0;
 }
