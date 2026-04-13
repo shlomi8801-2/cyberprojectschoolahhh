4ff54e454ed62b1aa7759731e8ea423e634e90e4
shlomi <shlomi8801@gmail.com>
2026-04-14 00:56:41 +0300

    added iterator for hashtable in arduino

--- client-controller/atmega328p/includes/Hashtable.h
+++ client-controller/atmega328p/includes/Hashtable.h
@@ -11,7 +11,21 @@ class Hashtable{
     LinkedList<Pair*> container;
     public:
     String& operator[](const String str);
+    inline unsigned int length() {return container.size();}
+    class Iterator{
+        unsigned int idx;
+        LinkedList<Pair*>* container;
+        public:
+        Iterator(unsigned int _idx,LinkedList<Pair*>* _container): idx(_idx),container(_container){}
+        inline void operator++(){
+        idx++;
+        }
+        inline bool operator!=(const Iterator& B){return this->idx != B.idx;}
+        inline Pair const &  operator*(){return *(container->get(idx));}
 
+    };
+    inline Iterator begin(){ return Iterator(0,&container);}
+inline Iterator end(){ return Iterator(length()-1,&container);}
     // inline Iteator<Pair> begin() noexcept { return container.begin(); }
     // inline VectorIterator<Pair> end() noexcept { return container.end(); }
     Hashtable();
--- client-controller/client.cpp
+++ client-controller/client.cpp
@@ -7,6 +7,7 @@
 #include "ATcommands.h"
 #include "client.h"
 #include "modemCore.h"
+#include "actionsService.h"
 #define DEBUG
 
 #include <Hashtable.h>
@@ -14,22 +15,27 @@
 int main()
 {
     Serial.begin(115200);
-
-
-
-
-
-
-
-
-    sei(); // start listening to interrupts
-    DDRB |= 1 << PORTB5; // sets builtin led to output mode
-    SoftwareSerial SerialAT(MODEM_RDX_PORT, MODEM_TDX_PORT); 
-    initialModem(&SerialAT);
-    conncectToSerevr();
-    //here after you achive a connection in single ip mode(default) you either send data or just in a loop waiting for data to be sent(data is pushed without warning)
+    Hashtable test;
+    test["hello"] = "REG";
+    int size =0;
+    // buildData(test,&size);
+    dbg((*test.begin()).key);
     
-    startInteractiveConsoleWithModem(SerialAT);
+
+
+
+
+
+
+
+    // sei(); // start listening to interrupts
+    // DDRB |= 1 << PORTB5; // sets builtin led to output mode
+    // SoftwareSerial SerialAT(MODEM_RDX_PORT, MODEM_TDX_PORT); 
+    // initialModem(&SerialAT);
+    // conncectToSerevr();
+    // //here after you achive a connection in single ip mode(default) you either send data or just in a loop waiting for data to be sent(data is pushed without warning)
+    
+    // startInteractiveConsoleWithModem(SerialAT);
 
     return 0;
 }
