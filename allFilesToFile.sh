cfa4e9ad389bcdcbdbde39a1090c19e832b8e556
shlomi <shlomi8801@gmail.com>
2026-05-12 14:56:24 +0300

    docs: changed the allFilesToFile.sh to use only files and not directories

--- allFilesToFile.sh
+++ allFilesToFile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find . -print0 | while read -d $'\0' file
+find . -print0 -type f | while read -d $'\0' file
 do
     echo "file: ${file#"./"}" #remove the first 2 characters that are "./"
     cat $file # print the file contents
